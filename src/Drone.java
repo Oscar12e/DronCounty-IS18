13c95c10b316da48f96c71fc5b5dc4cd8cd37a27
Paulo <paulozuniga17@gmail.com>
2018-04-07 11:49:07 -0600

    Se realizaron los objetos llamados: Drone, Viaje y Estacion

---
+++ src/Drone.java
@@ -0,0 +1,30 @@
+public class Drone {
+
+    private int largo;
+    private int ancho;
+    private int altura;
+    private int velocidad;
+
+    public Drone() {
+        this.largo = 2;
+        this.ancho = 2;
+        this.altura = 3;
+        this.velocidad = 120;
+    }
+
+    public int getLargo() {
+        return largo;
+    }
+
+    public int getAncho() {
+        return ancho;
+    }
+
+    public int getAltura() {
+        return altura;
+    }
+
+    public int getVelocidad() {
+        return velocidad;
+    }
+}
---
+++ src/Estacion.java
@@ -0,0 +1,18 @@
+public class Estacion {
+
+    private int cantDrones;
+    private int numeroEstacion;
+
+    public Estacion(int cantDrones, int numeroEstacion) {
+        this.cantDrones = cantDrones;
+        this.numeroEstacion = numeroEstacion;
+    }
+
+    public int getCantDrones() {
+        return cantDrones;
+    }
+
+    public int getNumeroEstacion() {
+        return numeroEstacion;
+    }
+}
