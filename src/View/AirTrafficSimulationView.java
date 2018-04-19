3a9ae254b56a7b2c59ed2babd79007678f635625
Oscar <oscarcortesc12@gmail.com>
2018-04-19 17:34:47 -0600

    Commit para solucionar un problema que se da al no los datos del view registrados en git (luego lo explico, no lo entiendo bien y quiero investigar más al respecto)

---
+++ src/View/AirTrafficSimulationView.java
@@ -0,0 +1,17 @@
+package View;
+import javafx.application.Application;
+import javafx.fxml.Initializable;
+import javafx.stage.Stage;
+import javafx.fxml.FXML;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AirTrafficSimulationView implements Initializable {
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
